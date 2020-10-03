23561c3702b46ffa10900423d56c25b5db18a28b
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2020-10-03 15:11:53 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function getAkanName(){
+    var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var maleAkanNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+    var femaleAkanNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+    var myBirthday = document.getElementById("dateofbirth").value;
+    var myGender = document.getElementsByName("gender");
+    var dateOfBirth = new Date(myBirthday);
+    var dayOfTheWeek = dateOfBirth.getDay();
+    if(myBirthday === ""){
+        document.getElementById('message').innerHTML = "<div> You Didn't Submit a Valid Date!</div>";
+    }
+    //checking if date given is not greater than actual date.
+
+    if (dateOfBirth >= new Date()){
+        console.log(myBirthday,new Date());
+        document.getElementById('message').innerHTML ="<span>Cannot assign names to unborns</span>";
+
+               }
+    else {
+        for(var i=0;i<myGender.length;i++){
+            if(myGender[i].checked){
+                if(myGender[i].value === "Male"){
+                    document.getElementById('message').innerHTML = " Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + maleAkanNames[dayOfTheWeek] + "</span>";
+                        
+            }
+                else {
+                    document.getElementById('message').innerHTML ="Born on a <span>" + days[dayOfTheWeek] + "</span>, Your Akan Name's <span>" + femaleAkanNames[dayOfTheWeek] + "</span>";
+                }
+                break;
+            }
+            else {
+                document.getElementById('message').innerHTML = "<div>Please select a Gender</div>";
+            }
+        }
+    }
+}
